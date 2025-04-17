18cd3d096f6cae0ba94c67a2f0183ca83c98126d
hagee <hagee.ktr@outlook.com>
2025-04-17 21:21:27 +0300

    Create router programmatic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () => {
+    setIsLoggedIn(true);
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  }
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/login")
+    }
+  }, [isLoggedIn])
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {isLoggedIn ? <NavBar logout={logout} /> : <NavBar logout={logout}/>}
+      <Outlet context={login}/>
     </div>
   );
 }
